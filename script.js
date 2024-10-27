b5c3069671a6fc4a157d108f83886efe2d3e1b4d
Rashmi K.C. <rashmikc707@gmail.com>
2024-10-27 09:14:07 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// console.log("helloworld")
+// const name = "Rashmi"
+// const message = `hello ${name}`
+// function greet(){
+//     console.log(`hello ${name}`);
+// }
+// greet("Ram");
+// console.log(message);
+// var global
+// const - doesnot change
+// const add = (a,b)=>{
+//     return a+b;
+// }
+// console.log(add(1,3)); arrow functions 
+// map
+// const number = [ 1,2,3,4,5];
+// const newNumber = number.map((num) => num*2);
+// console.log(newNumber)
+//filter
+// const evenNumbs = number.filter((num)=>num %2 != 0);
+// console.log(evenNumbs);
+// reduce
+// const sum = number.reduce((acc,num))=> acc+ num,0);
+// console.log(sum);
+// object and array destructuring
+// const person = { name:"rashmi" , age:"21" };
+// const{name,age} = person;
+// console.log(name);
+// console.log(age);
+// const colors = ["red", "green","blue"];
+// const [color1,, color3] = colors;
+// console.log(color1);
+// console.log(color3);
+// async and await 
+// async function getAllData(){
+//     console.log("data1");
+//     await getAllData(1);
+//     console.log("data1");
+//     await getAllData(2);
+//     console.log("data1");
+//     await getAllData(3);
+//     console.log("data1");
+//     await getAllData(4);
+//     console.log("data1");
+// }
+// console.log("one");
+// console.log("two");
+// setTimeout(() => {
+//     console.log("hello");
+// },4000);
+// console.log("three");
+// console.log("four");
+// CAll back 
+// call back hell
+// Promises : SOLUTION TO CALL BACK HELL
+// let promise = new Promise((RESOLVE,REJECT) =>{..}) 
+// pending
+// resolved, fulfied
+// reject
+// let promise = new Promise((resolve,reject) => {
+//     console.log("I am a Promise");
+//     reject("error")
+// })
